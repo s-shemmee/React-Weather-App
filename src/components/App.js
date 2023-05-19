d8e0ba49bdb1361da3698fc7b33f6779c5d23efd
s-shemmee <56132945+s-shemmee@users.noreply.github.com>
2023-05-19 17:51:10 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,122 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import SearchEngine from "./SearchEngine";
+import Forecast from "./Forecast";
+import "../styles.css";
+
+function App() {
+  const [query, setQuery] = useState();
+  const [weather, setWeather] = useState({
+    loading: true,
+    data: {},
+    error: false
+  });
+
+  const toDate = () => {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "Nocvember",
+      "December"
+    ];
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+
+    const currentDate = new Date();
+    const date = `${days[currentDate.getDay()]} ${currentDate.getDate()} ${
+      months[currentDate.getMonth()]
+    }`;
+    return date;
+  };
+
+  const search = async (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      setQuery("");
+      setWeather({ ...weather, loading: true });
+      const apiKey = "b03a640e5ef6980o4da35b006t5f2942";
+      const url = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}`;
+
+      await axios
+        .get(url)
+        .then((res) => {
+          console.log("res", res);
+          setWeather({ data: res.data, loading: false, error: false });
+        })
+        .catch((error) => {
+          setWeather({ ...weather, data: {}, error: true });
+          setQuery("");
+          console.log("error", error);
+        });
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const apiKey = "b03a640e5ef6980o4da35b006t5f2942";
+      const url = `https://api.shecodes.io/weather/v1/current?query=Rabat&key=${apiKey}`;
+
+      try {
+        const response = await axios.get(url);
+
+        setWeather({ data: response.data, loading: false, error: false });
+      } catch (error) {
+        setWeather({ data: {}, loading: false, error: true });
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>React Js Weather App</h1>
+
+      {/* SearchEngine component */}
+      <SearchEngine query={query} setQuery={setQuery} search={search} />
+
+      {weather.loading && (
+        <>
+          <br />
+          <br />
+          <h4>Loading...</h4>
+        </>
+      )}
+
+      {weather.error && (
+        <>
+          <br />
+          <br />
+          <span className="error-message">
+            <span style={{ fontFamily: "font" }}>
+              Sorry city not found, please try again.
+            </span>
+          </span>
+        </>
+      )}
+
+      {weather && weather.data && weather.data.condition && (
+        // Forecast component
+        <Forecast weather={weather} toDate={toDate} />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Forecast.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function Forecast({ weather }) {
+  const { data } = weather;
+  const [forecastData, setForecastData] = useState([]);
+
+  useEffect(() => {
+    const fetchForecastData = async () => {
+      const apiKey = "b03a640e5ef6980o4da35b006t5f2942";
+      const url = `https://api.shecodes.io/weather/v1/forecast?query=${data.city}&key=${apiKey}&units=metric`;
+
+      try {
+        const response = await axios.get(url);
+        setForecastData(response.data.daily);
+      } catch (error) {
+        console.log("Error fetching forecast data:", error);
+      }
+    };
+
+    fetchForecastData();
+  }, [data.city]);
+
+  const formatDay = (dateString) => {
+    const options = { weekday: "short" };
+    const date = new Date(dateString * 1000);
+    return date.toLocaleDateString("en-US", options);
+  };
+
+  const getCurrentDate = () => {
+    const options = {
+      weekday: "long",
+      day: "numeric",
+      month: "long",
+      year: "numeric"
+    };
+    const currentDate = new Date().toLocaleDateString("en-US", options);
+    return currentDate;
+  };
+
+  return (
+    <div>
+      <div className="city-name">
+        <h2>
+          {data.city}, <span>{data.country}</span>
+        </h2>
+      </div>
+      <div className="date">
+        <span>{getCurrentDate()}</span>
+      </div>
+      <div className="icon-temp">
+        {data.condition.icon_url && (
+          <img src={data.condition.icon_url} alt={data.condition.description} />
+        )}
+        {Math.round(data.temperature.current)}
+        <sup className="deg">°C</sup>
+      </div>
+      <div className="weather-info">
+        <p>{data.condition.description}</p>
+        <p>Wind speed: {data.wind.speed}m/s</p>
+      </div>
+      <div className="forecast">
+        <h3>5-Day Forecast:</h3>
+        <div className="forecast-container">
+          {forecastData &&
+            forecastData.slice(0, 5).map((day) => (
+              <div className="day" key={day.time}>
+                <p className="day-name">{formatDay(day.time)}</p>
+                {day.condition.icon_url && (
+                  <img
+                    className="day-icon"
+                    src={day.condition.icon_url}
+                    alt={day.condition.description}
+                  />
+                )}
+                <p className="day-temperature">
+                  {Math.round(day.temperature.day)}°C
+                </p>
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Forecast;
