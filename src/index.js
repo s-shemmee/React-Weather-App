3919844116065a0521fa84c4b1f086a7f2b8443d
chaimae <56132945+s-shemmee@users.noreply.github.com>
2023-05-21 19:53:31 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import { StrictMode } from "react";
 
 import App from "./components/App";
+import Credit from "./components/Credit";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
     <App />
+    <Credit />
   </StrictMode>,
   rootElement
 );
