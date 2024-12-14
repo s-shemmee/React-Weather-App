194da78ed317c4f1c3253225299d120a49f19ba2
s-shemmee <shemmee.dev@gmail.com>
2024-12-14 15:29:29 +0000

    Recreate app.js and Forecast.js

---
+++ src/components/App.js
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import SearchEngine from "./SearchEngine";
+import Forecast from "./Forecast";
+
+import "../styles.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({
+    loading: true,
+    data: {},
+    error: false,
+  });
+
+  const toDate = () => {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    const currentDate = new Date();
+    const date = `${days[currentDate.getDay()]} ${currentDate.getDate()} ${
+      months[currentDate.getMonth()]
+    }`;
+    return date;
+  };
+
+  const search = async (event) => {
+    event.preventDefault();
+    if (event.type === "click" || (event.type === "keypress" && event.key === "Enter")) {
+      setWeather({ ...weather, loading: true });
+      const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+      const url = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}`;
+
+      try {
+        const res = await axios.get(url);
+        setWeather({ data: res.data, loading: false, error: false });
+      } catch (error) {
+        setWeather({ ...weather, data: {}, error: true });
+        console.error("Error fetching weather data:", error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+      const url = `https://api.shecodes.io/weather/v1/current?query=Rabat&key=${apiKey}`;
+
+      try {
+        const response = await axios.get(url);
+        setWeather({ data: response.data, loading: false, error: false });
+      } catch (error) {
+        setWeather({ data: {}, loading: false, error: true });
+        console.error("Error fetching initial weather data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      {/* SearchEngine component */}
+      <SearchEngine query={query} setQuery={setQuery} search={search} />
+
+      {weather.loading && (
+        <>
+          <br />
+          <br />
+          <h4>Searching...</h4>
+        </>
+      )}
+
+      {weather.error && (
+        <>
+          <br />
+          <br />
+          <span className="error-message">
+            <span style={{ fontFamily: "font" }}>
+              Sorry, city not found. Please try again.
+            </span>
+          </span>
+        </>
+      )}
+
+      {weather && weather.data && weather.data.condition && (
+        // Forecast component
+        <Forecast weather={weather} toDate={toDate} />
+      )}
+    </div>
+  );
+}
+
+export default App;
